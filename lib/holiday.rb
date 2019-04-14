72c1517aa6bdced2553ff61eb32f24cace3facf4
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-14 22:15:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
    holiday.collect do |attribute, value|
       if value.include? ("BBQ")
         array = []
-        array.push(attribute).flatten
+        array.push(attribute).flatten.compact
       end
    end
   end
