4f2df08d86897e8e88f0286b4161abea8a9e5733
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-14 21:58:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
  holiday_hash.each do |season, holiday|
    holiday.each_pair do |attribute, value|
     if value == ["BBQ"]
-      puts attibute.to_sym
+      puts attribute.to_sym
     end
   end 
  end
