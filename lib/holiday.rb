bc864b2a909c3fc33aafebc016a0a854c973461c
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-14 22:32:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ holiday_hash.collect do |season, holiday|
   puts "#{season.to_s.capitalize}:"
 
   holiday.collect do |attribute, value|
-    attribute.to_s.tr("_", " ").capitalize
+    attribute = attribute.to_s.tr("_", " ").capitalize
     value.join(", ")
    puts "  #{attribute}: #{value}"
   end
